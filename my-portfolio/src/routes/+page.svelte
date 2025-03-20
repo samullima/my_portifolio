b54406a1690ca66ce379071364f4d376cf251e8f
samucaaaaaa <mucaspeedpvp@gmail.com>
2025-03-20 12:54:02 -0300

    feat: adicionando lab 4

---
+++ my-portfolio/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>
+    Samuel Lima
+</h1>
+
+<img src="images/Thorkell.png" alt="" height="400">
+
+<p>
+    Essa imagem mostra o Thorkell, um dos vikings mais fortes de Vinland Saga. <br>
+    Este é simplesmente um dos animes que eu mais gosto, pois tem uma história incrível!
+</p>
+    
