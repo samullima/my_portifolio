aeb9b6e930b43a5c7b488c7ab9d51cb94e8d7c5a
samucaaaaaa <mucaspeedpvp@gmail.com>
2025-04-08 13:10:31 -0300

    colocando ponto

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -21,7 +21,7 @@
     
     onMount(async () => {
     
-        data = await d3.csv("/loc.csv", row => ({
+        data = await d3.csv("./loc.csv", row => ({
             ...row,
             line: Number(row.line), // or just +row.line
             depth: Number(row.depth),
